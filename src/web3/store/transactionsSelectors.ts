e3b3a3ea44b7d388e0771741065ff3ecdcef5107
Alex <argeares97@gmail.com>
2022-11-25 13:14:25 +0200

    fix: get last tx

--- src/web3/store/transactionsSelectors.ts
+++ src/web3/store/transactionsSelectors.ts
@@ -23,25 +23,6 @@ export const selectTXByHash = <T extends BaseTx>(
   return state.transactionsPool[hash];
 };
 
-export const selectLastTxByTypeAndPayload = <T extends BaseTx>(
-  state: ITransactionsState<T>,
-  type: T['type'],
-  payload: T['payload']
-) => {
-  const allTransactions = selectAllTransactions(state);
-  const filteredTransactions = allTransactions.filter(
-    (tx) => tx.type === type && isEqual(tx.payload, payload)
-  );
-  const lastFilteredTransaction =
-    filteredTransactions[filteredTransactions.length - 1];
-
-  if (lastFilteredTransaction) {
-    return selectTXByHash(state, lastFilteredTransaction.hash);
-  } else {
-    return undefined;
-  }
-};
-
 export const selectAllTransactionsByWallet = <T extends BaseTx>(
   state: ITransactionsState<T>,
   from: string
@@ -54,4 +35,24 @@ export const selectPendingTransactionByWallet = <T extends BaseTx>(
   from: string
 ) => {
   return selectPendingTransactions(state).filter((tx) => tx.from == from);
+};
+
+export const selectLastTxByTypeAndPayload = <T extends BaseTx>(
+  state: ITransactionsState<T>,
+  from: string,
+  type: T['type'],
+  payload: T['payload']
+) => {
+  const allTransactions = selectAllTransactionsByWallet(state, from);
+  const filteredTransactions = allTransactions.filter(
+    (tx) => tx.type === type && isEqual(tx.payload, payload)
+  );
+  const lastFilteredTransaction =
+    filteredTransactions[filteredTransactions.length - 1];
+
+  if (lastFilteredTransaction) {
+    return selectTXByHash(state, lastFilteredTransaction.hash);
+  } else {
+    return undefined;
+  }
 };
