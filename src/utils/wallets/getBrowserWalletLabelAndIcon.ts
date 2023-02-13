ab059fe712bedaadd36a343d0cc55bd674d89a9e
Alex <argeares97@gmail.com>
2023-02-13 10:17:41 +0200

    fix: window.ethereum

--- src/utils/wallets/getBrowserWalletLabelAndIcon.ts
+++ src/utils/wallets/getBrowserWalletLabelAndIcon.ts
@@ -3,21 +3,25 @@ import { defaultWallet } from './wallets/defaultWallet';
 
 export function getBrowserWalletLabelAndIcon() {
   if (typeof window !== 'undefined') {
-    const userBrowserWallets = wallets.filter(
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      (wallet) => !!window.ethereum[wallet.identityFlag]
-    );
-    if (userBrowserWallets.length > 1 || userBrowserWallets.length === 0) {
-      return {
-        label: defaultWallet.label,
-        icon: defaultWallet.icon,
-      };
+    if (!!window.ethereum) {
+      const userBrowserWallets = wallets.filter(
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        (wallet) => !!window.ethereum[wallet.identityFlag]
+      );
+      if (userBrowserWallets.length > 1 || userBrowserWallets.length === 0) {
+        return {
+          label: defaultWallet.label,
+          icon: defaultWallet.icon,
+        };
+      } else {
+        return {
+          label: userBrowserWallets[0].label,
+          icon: userBrowserWallets[0].icon,
+        };
+      }
     } else {
-      return {
-        label: userBrowserWallets[0].label,
-        icon: userBrowserWallets[0].icon,
-      };
+      return undefined;
     }
   } else {
     return undefined;
