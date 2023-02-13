49d02b79bc4e55331e16824f35fe6f8bef3b15cb
Alex <argeares97@gmail.com>
2023-02-13 08:23:36 +0200

    fix: browser wallets details

--- src/utils/wallets/wallets.ts
+++ src/utils/wallets/wallets.ts
@@ -1,3 +1,93 @@
+import { alphawallet } from './wallets/alphawallet';
+import { atoken } from './wallets/atoken';
+import { binance } from './wallets/binance';
+import { bitkeep } from './wallets/bitkeep';
+import { bitpie } from './wallets/bitpie';
+import { bitski } from './wallets/bitski';
+import { blockwallet } from './wallets/blockwallet';
+import { brave } from './wallets/brave';
+import { coinbase } from './wallets/coinbase';
+import { core } from './wallets/core';
+import { dcent } from './wallets/dcent';
+import { defiwallet } from './wallets/defiwallet';
+import { detected } from './wallets/detected';
+import { enkrypt } from './wallets/enkrypt';
+import { exodus } from './wallets/exodus';
+import { frame } from './wallets/frame';
+import { frontier } from './wallets/frontier';
+import { gamestop } from './wallets/gamestop';
+import { huobiwallet } from './wallets/huobiwallet';
+import { hyperpay } from './wallets/hyperpay';
+import { imtoken } from './wallets/imtoken';
+import { liquality } from './wallets/liquality';
+import { mathwallet } from './wallets/mathwallet';
+import { meetone } from './wallets/meetone';
 import { metamask } from './wallets/metamask';
+import { mykey } from './wallets/mykey';
+import { okxwallet } from './wallets/okxwallet';
+import { oneInch } from './wallets/oneInch';
+import { opera } from './wallets/opera';
+import { ownbit } from './wallets/ownbit';
+import { phantom } from './wallets/phantom';
+import { rabby } from './wallets/rabby';
+import { rainbow } from './wallets/rainbow';
+import { safepal } from './wallets/safepal';
+import { sequence } from './wallets/sequence';
+import { status } from './wallets/status';
+import { tallywallet } from './wallets/tallywallet';
+import { tokenary } from './wallets/tokenary';
+import { tokenpocket } from './wallets/tokenpocket';
+import { tp } from './wallets/tp';
+import { trust } from './wallets/trust';
+import { walletio } from './wallets/walletio';
+import { xdefi } from './wallets/xdefi';
+import { zeal } from './wallets/zeal';
+import { zerion } from './wallets/zerion';
 
-export const wallets = [metamask];
+export const wallets = [
+  alphawallet,
+  atoken,
+  binance,
+  bitkeep,
+  bitpie,
+  bitski,
+  blockwallet,
+  brave,
+  coinbase,
+  core,
+  dcent,
+  defiwallet,
+  detected,
+  enkrypt,
+  exodus,
+  frame,
+  frontier,
+  gamestop,
+  huobiwallet,
+  hyperpay,
+  imtoken,
+  liquality,
+  mathwallet,
+  meetone,
+  metamask,
+  mykey,
+  okxwallet,
+  oneInch,
+  opera,
+  ownbit,
+  phantom,
+  rabby,
+  rainbow,
+  safepal,
+  sequence,
+  status,
+  tallywallet,
+  tokenary,
+  tokenpocket,
+  tp,
+  trust,
+  walletio,
+  xdefi,
+  zeal,
+  zerion,
+];
