f7ce4d63742f4517bab58095823e436fa10bac09
Timur <tima950203@gmail.com>
2023-08-01 14:57:53 +0200

    upgraded walletconnect v2

--- src/web3/connectors/index.ts
+++ src/web3/connectors/index.ts
@@ -3,7 +3,7 @@ import { initializeConnector } from '@web3-react/core';
 import { GnosisSafe } from '@web3-react/gnosis-safe';
 import { MetaMask } from '@web3-react/metamask';
 import { Connector } from '@web3-react/types';
-import { WalletConnect } from '@web3-react/walletconnect';
+import { WalletConnect as WalletConnectV2 } from '@web3-react/walletconnect-v2';
 
 import { ChainInformation } from '../../utils/chainInfoHelpers';
 import { ImpersonatedConnector } from './impersonatedConnector';
@@ -13,21 +13,28 @@ export type AllConnectorsInitProps = {
   chains: Record<number, ChainInformation>;
   urls: { [chainId: number]: string[] };
   desiredChainId: number;
+  wcProjectId: string;
 };
 
 export const initAllConnectors = (props: AllConnectorsInitProps) => {
+  const wcChains = Object.keys(props.chains).map(Number);
   const metaMask = initializeConnector<MetaMask>(
-    (actions) => new MetaMask({ actions })
+    (actions) => new MetaMask({ actions }),
   );
 
-  const walletConnect = initializeConnector<WalletConnect>(
+  const walletConnect = initializeConnector<WalletConnectV2>(
     (actions) =>
-      new WalletConnect({
+      new WalletConnectV2({
         actions,
         options: {
-          rpc: props.urls,
+          projectId: props.wcProjectId,
+          chains: wcChains,
+          showQrModal: true,
         },
-      })
+        onError: (error) => {
+          console.error('walletconnect error', error);
+        },
+      }),
   );
 
   const coinbase = initializeConnector<CoinbaseWallet>(
@@ -65,7 +72,7 @@ export type WalletType =
 
 export function getConnectorName(connector: Connector): WalletType | undefined {
   if (connector instanceof MetaMask) return 'Metamask';
-  if (connector instanceof WalletConnect) return 'WalletConnect';
+  if (connector instanceof WalletConnectV2) return 'WalletConnect';
   if (connector instanceof CoinbaseWallet) return 'Coinbase';
   if (connector instanceof GnosisSafe) return 'GnosisSafe';
   if (connector instanceof ImpersonatedConnector) return 'Impersonated';
