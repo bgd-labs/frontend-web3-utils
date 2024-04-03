34657973435529c16c20fdc4bfe886aba0d77e27
Alex <argeares97@gmail.com>
2024-04-03 15:09:30 +0300

    fix: wagmi zustand sync

--- src/web3/components/WagmiZustandSync.tsx
+++ src/web3/components/WagmiZustandSync.tsx
@@ -1,47 +1,38 @@
 import { Config, GetAccountReturnType, watchAccount } from '@wagmi/core';
 import { useEffect } from 'react';
-import { StoreApi, UseBoundStore } from 'zustand';
 
 interface WagmiZustandSyncProps {
   wagmiConfig: Config;
   defaultChainId?: number;
-  useStore: UseBoundStore<
-    StoreApi<{
-      setWagmiConfig: (config: Config) => void;
-      changeActiveWalletAccount: (
-        account?: GetAccountReturnType,
-      ) => Promise<void>;
-      setDefaultChainId: (chainId: number) => void;
-    }>
-  >;
+  store: {
+    setWagmiConfig: (config: Config) => Promise<void>;
+    changeActiveWalletAccount: (
+      account?: GetAccountReturnType,
+    ) => Promise<void>;
+    setDefaultChainId: (chainId: number) => void;
+  };
 }
 
 export function WagmiZustandSync({
   wagmiConfig,
   defaultChainId,
-  useStore,
+  store,
 }: WagmiZustandSyncProps & {
   wagmiConfig: Config;
 }) {
-  const setDefaultChainId = useStore((store) => store.setDefaultChainId);
-  const setWagmiConfig = useStore((store) => store.setWagmiConfig);
-  const changeActiveWalletAccount = useStore(
-    (store) => store.changeActiveWalletAccount,
-  );
-
   useEffect(() => {
     if (defaultChainId) {
-      setDefaultChainId(defaultChainId);
+      store.setDefaultChainId(defaultChainId);
     }
   }, [defaultChainId]);
 
   useEffect(() => {
-    setWagmiConfig(wagmiConfig);
+    store.setWagmiConfig(wagmiConfig);
   }, [wagmiConfig]);
 
   watchAccount(wagmiConfig, {
     onChange: async (account) => {
-      await changeActiveWalletAccount(account);
+      await store.changeActiveWalletAccount(account);
     },
   });
 
